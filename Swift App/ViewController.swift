610cb1f9a9a2b6ddec50adb21e1037c1bf1a8a6d
Daniel Bonaparte <danielbonaparte@Daniels-MacBook-Pro.local>
2017-02-23 08:58:43 -0800

    Boolean fun

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var coolLabel: UILabel!
     
     @IBOutlet var text1: UITextField!
@@ -19,21 +19,28 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
-        coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = true
+        
+        if addition {
+            coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            coolLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
